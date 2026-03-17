43bd0d89fb845160c488c33d2b6ee503e60c9d35
AntoineLandois <127930774+AntoineLandois@users.noreply.github.com>
2026-03-17 09:53:53 +0100

    Translate French messages to English in install.sh

--- infra/tetragon/install.sh
+++ infra/tetragon/install.sh
@@ -6,22 +6,22 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 PROJECT_INFRA=$(dirname "$SCRIPT_DIR")
 
 echo "========================================================"
-echo "🛡️  DÉPLOIEMENT INFRASTRUCTURE TETRAGON & EBPF"
+echo "DÉPLOIEMENT INFRASTRUCTURE TETRAGON & EBPF"
 echo "========================================================"
 
 echo "🚀 1. CLUSTER KIND"
 if kind get clusters | grep -q "^ebpf-lab$"; then
-    echo "⚠️  Cluster déjà présent."
+    echo "CLUSTER ALREADY HERE"
 else
     kind create cluster --config "$PROJECT_INFRA/k8s/kind-config.yaml" --name ebpf-lab
 fi
 
-echo "📦 2. HELM INSTALL"
+echo "2. HELM INSTALL"
 helm repo add cilium https://helm.cilium.io > /dev/null 2>&1
 helm repo update > /dev/null 2>&1
 helm upgrade --install tetragon cilium/tetragon -n kube-system --set tetragon.hostNetwork=true
 
-echo "⏳ 3. ATTENTE PODS"
+echo "3. WAITING PODS"
 # On attend que le pod soit au moins créé
 until kubectl get pods -n kube-system -l app.kubernetes.io/name=tetragon 2>/dev/null | grep -q "tetragon"; do
     echo "Recherche du Pod Tetragon..."
@@ -30,7 +30,7 @@ done
 # On attend qu'il soit prêt
 kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=tetragon -n kube-system --timeout=120s
 
-echo "🛡️  4. ENREGISTREMENT CRD & POLITIQUES"
+echo "4. ENREGISTREMENT CRD & POLITIQUES"
 # BOUCLE CRUCIALE : On attend que le CRD soit reconnu par l'API
 echo "Attente de l'enregistrement des CRD Tetragon..."
 MAX_RETRIES=10
@@ -38,7 +38,7 @@ COUNT=0
 until kubectl get crd tracingpolicies.cilium.io > /dev/null 2>&1; do
     COUNT=$((COUNT+1))
     if [ $COUNT -ge $MAX_RETRIES ]; then
-        echo "❌ Erreur : Le CRD n'a pas pu être installé à temps."
+        echo "ERROR : The CRD could not be installed in time."
         exit 1
     fi
     echo "Tentative $COUNT/$MAX_RETRIES : CRD non trouvé, attente..."
@@ -51,12 +51,12 @@ kubectl wait --for condition=established --timeout=60s crd/tracingpolicies.ciliu
 if [ -d "$PROJECT_INFRA/policies/" ]; then
     echo "Application des politiques..."
     kubectl apply -f "$PROJECT_INFRA/policies/"
-    echo "✅ Politiques appliquées."
+    echo "Policies are apply"
 else
-    echo "❌ Erreur : Dossier policies introuvable."
+    echo "Error : Policies folder not found"
     exit 1
 fi
 
 echo "========================================================"
-echo "🎉 INFRA PRÊTE"
+echo "INFRA READY"
 echo "========================================================"
